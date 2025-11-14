65ef7d148b5ea8977ea77d3b41b1614137e3bac1
Tushar Bhadwaj <tusharrbhardwaj@gmail.com>
2025-11-14 03:55:29 +0100

    Updated Reading system as of CSV files and date
    This file is now updated from txt reading format to csv reading system.
    It now detects date form the provided data sheet to confirm the date of attendace.

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-def ReadFile():
-    with open(name,'r') as attendee:
-        
-        data = attendee.read()
-        print(data)
+import csv
+import re
 
+def ReadFile():
+    
+    with open(name,'r') as attendee:
+        reader = csv.reader(attendee)
+        next(reader)
+        
+        data = [row for row in reader]
+         
+        raw_date = data[0][0]
+        date = attendance_date(raw_date)
+        
+        print("We are marking Attendance for Date :", date)
+        
+        student_present = [students[-2] for students in data]
+        student_present.sort()
+        
+        print(*student_present, sep='\n')
+
+    print("Total Students present in class : ",len(student_present))
 
 def validation():
     try:
@@ -13,11 +29,19 @@ def validation():
         print("Try Entering the Correct Name of the Attendance File")
         main()
         
+
+def attendance_date(raw_date):
+    
+    pattern = re.compile(r"\d\d\d\d-\d\d-\d\d")
+    date = re.search(pattern, raw_date)
+    
+    return date.group()
+    
         
 def main():
     global name
     name = input('Enter the name : ')
-    name = name+'.txt'
+    name = name+'.csv'
     
     if validation():
         ReadFile()
