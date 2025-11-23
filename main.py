aa655a9a5a30895438d24273869fa421f93f13d4
Tushar Bhadwaj <tusharrbhardwaj@gmail.com>
2025-11-23 02:30:55 +0100

    Added features to validate date with the data
    This helps to reduce data error, the date of the data would once be matched to the date in the data to ensure the date matching for the attendace.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import csv
 import re
+from datetime import date
 
 def ReadFile():
     
@@ -8,11 +9,14 @@ def ReadFile():
         next(reader)
         
         data = [row for row in reader]
-         
+        global raw_date
         raw_date = data[0][0]
-        date = attendance_date(raw_date)
+        validated_date = date_validation()
+
+        data_date = validated_date[0]
+        choice = validated_date[1]
         
-        print("We are marking Attendance for Date :", date)
+        print("We are marking Attendance for Date :", data_date)
         
         student_present = [students[-2] for students in data]
         student_present.sort()
@@ -20,6 +24,9 @@ def ReadFile():
         print(*student_present, sep='\n')
 
     print("Total Students present in class : ",len(student_present))
+    
+    if choice in 'Yy':
+        print("Opening the broswer to mark the attendance")
 
 def validation():
     try:
@@ -28,14 +35,25 @@ def validation():
     except FileNotFoundError:
         print("Try Entering the Correct Name of the Attendance File")
         main()
+    
+def date_validation():
+    def current_date():
+        todays_date =   date.today()
+        return todays_date
+    current_date = current_date()
+    
+    def attendance_date(raw_date):
+        pattern = re.compile(r"\d\d\d\d-\d\d-\d\d")
+        data_date = re.search(pattern, raw_date)
+        return data_date.group()
+    attendance_date = attendance_date(raw_date)
+    
+    if (current_date != attendance_date):
+        print("The Date of the data does not matches to today's date !")
+        print(f"Today's Date is {current_date}, While the data we are working with is of date {attendance_date}")
+        choice = input("Are you sure you still want to continue with the current Data-File? : ")
         
-
-def attendance_date(raw_date):
-    
-    pattern = re.compile(r"\d\d\d\d-\d\d-\d\d")
-    date = re.search(pattern, raw_date)
-    
-    return date.group()
+    return attendance_date, choice
     
         
 def main():
@@ -43,8 +61,10 @@ def main():
     name = input('Enter the name : ')
     name = name+'.csv'
     
+    
     if validation():
         ReadFile()
+        
 
 if __name__ == '__main__':
     
