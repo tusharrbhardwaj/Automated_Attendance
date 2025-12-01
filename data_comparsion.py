85cf09a302902961bebcf65b6ed6d16141451e97
Tushar Bhadwaj <tusharrbhardwaj@gmail.com>
2025-12-01 19:37:03 +0100

    Orgranizing files based on date and module code
    new function: organizer(date, module_code) has been added to organize files into folders based on date and module code.
    This function creates a folder named after the date and moves all files starting with the module code and ending with .txt into that folder.

--- data_comparsion.py
+++ data_comparsion.py
@@ -1,5 +1,16 @@
+'''
+This script contains two main functions: comparison() and organizer().
+comparison() compares the data extracted from the module's student list and the attendance list provided by the user.
+It then creates two separate files named 'present.txt' and 'absent.txt' to store
+the names of present and absent students respectively.
+organizer() creates seperate folders as per the date of attendance and moves the respective files into those folders.
+'''
+#os is used to create folders and move files into those folders
 import os
+#coloorama is used to make the output in terminal colorful
+from colorama import Fore as color
 
+#present and absent are global lists to store names of present and absent students respectively.
 present = []
 absent=[]
 
@@ -14,6 +25,7 @@ def comparison(attendance_file_name,file_name):
         with open(attendance_file_name) as attendance:
             attendance_list = attendance.read().splitlines()
 
+            #answer stores the total number of students present in the class out of total strength
             answer = (f"total student present in class {len(attendance_list)}/{len(total_list)}")
             print(answer)
             
@@ -35,5 +47,32 @@ def comparison(attendance_file_name,file_name):
             absent_file.write(names+'\n')
         
 
-# comparison('B101A_students_list.txt','B101A_attendance_list.txt')
-# print("Files 'present.txt' and 'absent.txt' created successfully.")
+def organizer(module_code,date):
+    '''
+    organizer() creates seperate folders as per the date of attendance and moves the respective files into those folders.
+    '''
+    try:
+        '''
+         folder_name is created using date variable passed to the function.
+         If the folder already exists, it directly moves the files into that folder.
+         '''
+         
+         #folder_name is created using date variable passed to the function.
+         #If the folder already exists, it directly moves the files into that folder.
+        folder_name = date
+        if not os.path.exists(folder_name):
+            os.makedirs(folder_name)
+            for file in os.listdir():
+                if file.startswith(module_code) and file.endswith('.txt'):
+                    '''
+                     It moves all the files starting with module_code and ending with .txt into the created folder.
+                '''
+                    os.rename(file,os.path.join(folder_name,file))
+        else:
+            for file in os.listdir():
+                if file.startswith(module_code) and file.endswith('.txt'):
+                    os.rename(file,os.path.join(folder_name,file))
+        print(color.GREEN + "Files organized successfully.")
+                
+    except:
+        print(color.RED + "There was some error in organizing the files.")
