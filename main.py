8130e379c7d7eaa3b2f0e1a8a86dc1b4a383fb51
Tushar Bhadwaj <tusharrbhardwaj@gmail.com>
2025-12-01 16:01:41 +0100

    Documented Code and Polished definitaion

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
+'''
+main.py is the combined file which uses all the other modules to create a complete automated 
+attendance marking system.
+It first checks for the existence of config.json to retrieve login credentials from it using 
+automation.login_data().
+Then it calls automation.startup() to awaken all the necessary utilities required by 
+selenium to execute browser automation.
+Then it calls automation.login() to navigate to and enter data into website's login input feilds.
+After that it calls automation.login_verification() to verfies if login happened or not by checking a 
+random element which would only be available if login was successfull.
+If login is successfull, it asks for module code input from user and calls automation.navigate_module() 
+to navigate to that module.
+Then it calls automation.navigating_student() to navigate to the students list of that module.
+After that it calls automation.data_extraction() to extract the students data and save it into a csv file.
+If at any point there is an error, it prints appropriate error message to the user.
+If config.json does not exist, it asks the user if they want to use initializer.py to create it.
+If user agrees, it calls initializer.main() to create config.json.  
+'''
+
+#import os module to check for existence of config.json
 import os
+#import color module from colorama for colored terminal outputs
 from colorama import Fore as color
+#import initializer module to create config.json if it does not exist and retrieve login credentials from it
 import initializer
-import pyinputplus as pyin
+#import automation module to perform all the browser automation tasks
 import automation
+#import pyinputplus module to take validated inputs from user.
+import pyinputplus as pyin
+
 
 try:
     '''
@@ -16,20 +41,35 @@ try:
             #automation.startup() only needs to be called once in whole program
             automation.startup() #startup() awkens all the necessary utilities required by selenium to execute browser automation
             automation.login(eid,pwd) #login() navigates to and enter data into website's login input feilds (refer to automation.py)
-            login_verification = automation.login_verification() #login_verification() verfies if login happened or not by checking a random element which would only be available if login was successfull
+            #login_verification() verfies if login happened or not by checking a random element which would only be available if login was successfull
+            login_verification = automation.login_verification() #login_verification() returns True if login is successfull otherwise, raises an error
         
             if login_verification == True:
-            
+                '''
+                if login is successfull, the program asks for module code input from user and calls
+                navigate_module() to navigate to that module.
+                Then it calls navigating_student() to navigate to the students list of that module.
+                After that it calls data_extraction() to extract the students data and save it into a csv file.
+                '''
+                #module_code takes module code input from user in uppercase format to avoid case sensitivity issues
                 module_code = input(color.WHITE + "Hello Professor ! Please Enter the Module Code You want to mark attendance for : ").upper()
                 try:
                     print(color.CYAN+"Navigating to the module ...")
                     navigated_module = automation.navigate_module(module_code)
+                    '''
+                    navigate_module() tries to navigate to the module specified by module_code input from user and returns True 
+                    if navigation is successfull otherwise, raises an error
+                    '''
                 except:
                     print(color.RED + "Could not find the module, please check the module code once and try again")
             
                 if navigated_module:
+                    '''
+                    if module navigation is successfull, it tries to navigate to students list and extract data from there.
+                    '''
                     print(color.GREEN+ f"Navigated to the module {module_code} successfully.")
                     try:
+                        #navigating_student() tries to navigate to the students list of the module and returns True if navigation is successfull otherwise, raises an error
                         navigated_students = automation.navigating_student()
                         print(color.GREEN+ "Navigated to the students list sucessfully.")
                     except:
@@ -37,7 +77,11 @@ try:
 
                     
                     if navigated_students:
+                        '''
+                        if navigation to students list is successfull, it tries to extract data from there and save it into a csv file.
+                        '''
                         try:
+                            #file_name is created using module_code to avoid confusion between multiple modules data files
                             file_name = module_code+"_students_list.csv"
                             automation.data_extraction(file_name)
                             print(color.GREEN+f"File saves successfully as '{file_name}'.")
@@ -47,8 +91,9 @@ try:
                         print(color.RED + "There was some error navigating to the students list.")
                     
                 else:
-                    navigated_module = module_code = input(color.WHITE + "Hello Professor ! Please Enter the Module Code You want to mark attendance for : ")
-                    automation.navigate_module(module_code)
+                    module_code = input(color.WHITE + "Hello Professor ! Please Enter the Module Code You want to mark attendance for : ")
+                    #navigaed_module stores the return value from navigate_module() to ensure the progress of the function even after initial failure
+                    navigated_module =automation.navigate_module(module_code)
             
             else:
                 '''
@@ -70,6 +115,7 @@ try:
     else:
         print(color.RED+ "We could not find any file named 'config.json'")
         print("I would recommend you to use initializer.py first, want to use it? : ")
+        #config_creation takes validated yes/no input from user using pyinputplus module
         config_creation = pyin.inputYesNo(color.WHITE)
         if config_creation == 'yes':
             print()
