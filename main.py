544a0a018290eca0f81b8ea7b5db3f610801d103
Tushar Bhadwaj <tusharrbhardwaj@gmail.com>
2025-12-01 02:07:16 +0100

    main.py() now is fully intergated wid initializer
    This would help user to run the main program only instead of running here and there for right direction.
    This program will automatically redirect the user to required program file depending on their response
    to choice questions.

--- main.py
+++ main.py
@@ -1,72 +1,24 @@
-import csv
-import re
-from datetime import date
+from colorama import Fore as color
+import initializer
+import pyinputplus as pyin
 
-def ReadFile():
+try:
     
-    with open(name,'r') as attendee:
-        reader = csv.reader(attendee)
-        next(reader)
-        
-        data = [row for row in reader]
-        global raw_date
-        raw_date = data[0][0]
-        validated_date = date_validation()
-
-        data_date = validated_date[0]
-        choice = validated_date[1]
-        
-        print("We are marking Attendance for Date :", data_date)
-        
-        student_present = [students[-2] for students in data]
-        student_present.sort()
-        
-        print(*student_present, sep='\n')
-
-    print("Total Students present in class : ",len(student_present))
-    
-    if choice in 'Yy':
-        print("Opening the broswer to mark the attendance")
-
-def validation():
+    '''
+    The first and formost thing in the program is to check whether config.json exists to reterieve login
+    credentials from it.
+    '''
     try:
-        if open(name,'r'):
-            return True
-    except FileNotFoundError:
-        print("Try Entering the Correct Name of the Attendance File")
-        main()
-    
-def date_validation():
-    def current_date():
-        todays_date =   date.today()
-        return todays_date
-    current_date = current_date()
-    
-    def attendance_date(raw_date):
-        pattern = re.compile(r"\d\d\d\d-\d\d-\d\d")
-        data_date = re.search(pattern, raw_date)
-        return data_date.group()
-    attendance_date = attendance_date(raw_date)
-    
-    if (current_date != attendance_date):
-        print("The Date of the data does not matches to today's date !")
-        print(f"Today's Date is {current_date}, While the data we are working with is of date {attendance_date}")
-        choice = input("Are you sure you still want to continue with the current Data-File? : ")
-        
-    return attendance_date, choice
-    
-        
-def main():
-    global name
-    name = input('Enter the name : ')
-    name = name+'.csv'
-    
-    
-    if validation():
-        ReadFile()
-        
-
-if __name__ == '__main__':
-    
-    main()
-    
+        open('config.json')  
+    except:
+        print(color.RED+ "We could not find any file named 'config.json'")
+        print("I would recommend you to use initializer.py first, want to use it? : ")
+        config_creation = pyin.inputYesNo(color.WHITE)
+        if config_creation == 'yes':
+            print()
+            initializer.main()
+        else:
+            print("It's understandable! Bye then :)")
+            
+except:
+    print("There might be some missing files or modules. please check them ones and try again.")
